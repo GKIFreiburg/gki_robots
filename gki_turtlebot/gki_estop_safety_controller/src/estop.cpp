01ee850479556999490a4af4f1938c02dfcc2d0f
viki <viki@ROS.(none)>
2013-06-21 15:55:40 +0200

    added gki_estop_safety_controller package

---
+++ gki_turtlebot/gki_estop_safety_controller/src/estop.cpp
@@ -0,0 +1,46 @@
+#include <ros/ros.h>
+#include <std_msgs/Bool.h>
+#include <turtlebot_button/Buttons.h>
+#include <geometry_msgs/Twist.h>
+
+
+ros::Publisher pub;
+
+
+void estop(const std_msgs::Bool input)
+{
+    if (input.data)
+    {
+        geometry_msgs::Twist cmd_msg;
+        cmd_msg.linear.x = 0;
+        cmd_msg.linear.y = 0;
+        cmd_msg.linear.z = 0;
+        cmd_msg.angular.x = 0;
+        cmd_msg.angular.y = 0;
+        cmd_msg.angular.z = 0;
+        pub.publish(cmd_msg);
+    }
+}
+
+//fraglich for no data
+void etstop()
+{
+
+}
+
+int main (int argc, char **argv){
+    //Initialize ROS
+    ros::init(argc, argv, "box_dector");
+    ros::NodeHandle nh;
+
+
+    // Create a ROS subscriber for the input point cloud
+    ros::Subscriber sub = nh.subscribe ("/estop", 1, estop);
+
+    // Create a ROS publisher for the output point cloud
+    pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux", 1);
+
+
+    // Spin
+    ros::spin ();
+}
