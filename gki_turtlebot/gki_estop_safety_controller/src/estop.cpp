e36ed15881b0d60bfd0fd33ef6e1d10206982345
Christian Dornhege <dornhege@informatik.uni-freiburg.de>
2014-02-11 18:23:21 +0100

    Cleaned gki_estop_controller

--- gki_turtlebot/gki_estop_safety_controller/src/estop.cpp
+++ gki_turtlebot/gki_estop_safety_controller/src/estop.cpp
@@ -1,83 +1,64 @@
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
-#include <turtlebot_button/Buttons.h>
 #include <geometry_msgs/Twist.h>
-
 
 ros::Publisher pub;
 bool data_available = false;
 
+// EStop Safety Controller
+// Stop the robot if estop doesn't signal GO
+// Be silent otherwise.
+
+void stop_robot()
+{
+    geometry_msgs::Twist cmd_msg;
+    cmd_msg.linear.x = 0;
+    cmd_msg.linear.y = 0;
+    cmd_msg.linear.z = 0;
+    cmd_msg.angular.x = 0;
+    cmd_msg.angular.y = 0;
+    cmd_msg.angular.z = 0;
+    pub.publish(cmd_msg);
+}
 
 void estop(const std_msgs::Bool input)
 {
-    if (!input.data)
-    {
-        geometry_msgs::Twist cmd_msg;
-        cmd_msg.linear.x = 0;
-        cmd_msg.linear.y = 0;
-        cmd_msg.linear.z = 0;
-        cmd_msg.angular.x = 0;
-        cmd_msg.angular.y = 0;
-        cmd_msg.angular.z = 0;
-        pub.publish(cmd_msg);
+    if(!input.data) {
+        stop_robot();
+        ROS_WARN_THROTTLE(5.0, "Stopping Robot due to EStop pressed.");
     }
 
     data_available = true;
 }
 
-int main (int argc, char **argv){
-    //Initialize ROS
+int main (int argc, char **argv)
+{
     ros::init(argc, argv, "estop_safety_controller");
     ros::NodeHandle nh;
 
-    //edit path!!!
-    int safety_level = 1;
-    //nh.getParam("gki_estop_safety_controller/../safety_level",safety_level);
+    ros::NodeHandle nhPriv("~");
 
-    // Create a ROS subscriber for estop topic
+    bool auto_estop = true;
+    nh.param("auto_estop", auto_estop, auto_estop);
+
     ros::Subscriber sub = nh.subscribe ("/estop", 1, estop);
-
-    // Create a ROS publisher for the output estop
     pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/estop", 1);
 
-
-    //check with 1Hz
-    ros::Rate r(1);   //1Hz
-
-    while (1)
-    {
-        //switch over safety level
-        switch(safety_level)
-        {
-        case 0:
-            if(!data_available)
-            {
-                ROS_WARN_STREAM("STOPPING WHILE THERE WAS NO ESTOP DATA!");
-                geometry_msgs::Twist cmd_msg;
-                cmd_msg.linear.x = 0;
-                cmd_msg.linear.y = 0;
-                cmd_msg.linear.z = 0;
-                cmd_msg.angular.x = 0;
-                cmd_msg.angular.y = 0;
-                cmd_msg.angular.z = 0;
-                pub.publish(cmd_msg);
+    ros::Rate r(1);   //    1Hz
+    while (ros::ok()) {
+        if(!data_available) {
+            if(auto_estop) {
+                ROS_WARN_THROTTLE(1.0, "No EStop data available - Stopping robot!");
+                stop_robot();
+            } else {
+                ROS_WARN_THROTTLE(1.0, "No EStop data available!");
             }
-            break;
-        case 1:
-            if(!data_available)
-            {
-                ROS_WARN_STREAM("NO ESTOP DATA!");
-            }
-            break;
         }
 
-        //reset value for available data
         data_available = false;
 
         ros::spinOnce();
         r.sleep();
     }
-
-    // Spin
-    ros::spin ();
 }
+
